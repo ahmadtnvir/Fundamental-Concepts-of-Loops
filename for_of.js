1b4c7b5c0c892ca37ce97a9a3ade635c87b58e74
Tanvir Ahmad <tnvir9@gmail.com>
2024-09-02 10:32:45 +0600

    while loop

--- for_of.js
+++ for_of.js
@@ -1,11 +1,15 @@
 const rollNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 for (const rollNumber of rollNumbers) {
-    console.log(rollNumber);
+  console.log(rollNumber); //Output: 1, 2, 3, 4, 5, 6, 7, 8, 9
 }
 
 const fruits = ["Orange", "Apple", "Jackfruit", "Dragon", "Banana"];
 
 for (const fruit of fruits) {
-    console.log(fruit);
+  console.log(fruit); // Outputs: Orange,Apple,Jackfruit,Dragon,Banana
 }
+// Expected output: "a"
+// Expected output: "b"
+// Expected output: "c"
+
---
+++ while.js
@@ -0,0 +1,12 @@
+const num = 7;
+if(num < 10){
+    console.log("Choto akta number pice");
+}
+console.log("Something ");
+
+
+let num1 = 0;
+while(num1 < 5){
+    console.log("object number", num1);
+    num1++;
+}

9e1e10b94741d6b80244eecb4b94c90a85f4207c
Tanvir Ahmad <tnvir9@gmail.com>
2024-09-02 10:02:06 +0600

    first commit

---
+++ for_of.js
@@ -0,0 +1,11 @@
+const rollNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for (const rollNumber of rollNumbers) {
+    console.log(rollNumber);
+}
+
+const fruits = ["Orange", "Apple", "Jackfruit", "Dragon", "Banana"];
+
+for (const fruit of fruits) {
+    console.log(fruit);
+}

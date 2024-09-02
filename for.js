a29b2e254057a61c63d1e64c8046a8b9873f751f
Tanvir Ahmad <tnvir9@gmail.com>
2024-09-02 11:55:16 +0600

    for.js

---
+++ for.js
@@ -0,0 +1,41 @@
+// ! Difference between While Loop and For Loop
+
+// While Loop
+// let num1 = 1;
+// while (num1 <= 10) {
+//   console.log(num1);
+//   num1++;
+// }
+
+// For Loop
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// ! Even Number using for loop
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+//   if (i % 2 === 0) {
+//     console.log("Even Number ", i);
+//   }
+// }
+
+// ? OR
+// for (let i = 2; i <= 10; i += 2) {
+//   console.log(i);
+// }
+
+// ! Odd Number using for loop
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+//   if (i % 2 !== 0) {
+//     console.log("Odd Number ", i);
+//   }
+// }
+
+// ? OR
+// for (let i = 1; i <= 10; i += 2) {
+//   console.log(i);
+// }
